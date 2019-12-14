f0604bf02f492fde557313f3a7f65832408db079
Kevin <z547607118@163.com>
2019-12-15 00:05:51 +0800

    update

---
+++ vue_app/src/components/GoodsCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div
+      v-for="item in goods"
+      :key="item.index"
+    >
+      <van-row>
+        <van-card
+          :num="item.number"
+          :price="item.price"
+          :desc="item.describe"
+          :title="item.goodName"
+          thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+        />
+      </van-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Row, Col } from 'vant';
+import { Card } from 'vant';
+
+Vue.use(Card);
+
+Vue.use(Row).use(Col);
+
+    export default {
+        name: 'GoodsCard',
+        data() {
+            return {
+                
+            }
+        },
+        props: {
+          goods:{
+            type: Array,
+            default: null,
+          },
+        },
+    }
+</script>
+
+<style lang="" scoped>
+    
+</style>
--- vue_app/src/views/AllItem.vue
+++ vue_app/src/views/AllItem.vue
@@ -1,17 +1,52 @@
 <template>
-  <div>所有商品</div>
+  <div>
+    <div>
+      <van-search
+        placeholder="请输入搜索关键词"
+        v-model="search"
+        shape="round"
+      />
+    </div>
+    <div>
+      <GoodsCard
+        :goods="goodsList"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: '',
-        data() {
-            return {
-                
-            }
-        }
-    }
+import Vue from 'vue';
+import GoodsCard from '../components/GoodsCard'
+import axios from 'axios';
+import { Search } from 'vant';
+
+
+Vue.use(Search);
+
+export default {
+    name:'AllItem',
+    components: {
+        GoodsCard: GoodsCard,
+    },
+    data() {
+        return {
+            search: '',
+            goodsList: [{"goodId":4,"userId":"001","goodName":"篮球","number":1,"price":88,"describe":"好！","phone1":null,"phone2":null,"phone3":null},{"goodId":6,"userId":"009","goodName":"足球","number":1,"price":55,"describe":"好！","phone1":null,"phone2":null,"phone3":null},{"goodId":7,"userId":"005","goodName":"电脑","number":1,"price":2000,"describe":null,"phone1":null,"phone2":null,"phone3":null},{"goodId":8,"userId":"008","goodName":"电脑","number":1,"price":3000,"describe":null,"phone1":null,"phone2":null,"phone3":null}],
+        }     
+    },
+    methods: {
+      get_allGoods(){
+        axios.get('http://localhost:8090/androidApp/Goods/FindAllGoods')
+        .then(response=>{
+          // eslint-disable-next-line no-console
+          console.log(response);
+        })
+      }
+    },
+}
 </script>
+
 
 <style>
     
