66ee63525960a0cb0bf12dcf7de27b88bb039ab1
Kevin <z547607118@163.com>
2019-12-15 20:23:39 +0800

    Update SellCard.vue

--- vue_app/src/components/SellCard.vue
+++ vue_app/src/components/SellCard.vue
@@ -11,7 +11,30 @@
           :desc="item.describe"
           :title="item.goodName"
           thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
-        />
+        >
+          <div slot="tags">
+            <van-tag
+              plain
+              type="danger"
+            >
+              标签
+            </van-tag>
+            <van-tag
+              plain
+              type="danger"
+            >
+              标签
+            </van-tag>
+          </div>
+          <div slot="footer">
+            <van-button size="mini">
+              按钮
+            </van-button>
+            <van-button size="mini">
+              按钮
+            </van-button>
+          </div>
+        </van-card>
       </van-row>
     </div>
   </div>
